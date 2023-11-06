244f695fefdb081fb72cf9995693837628b88568
ED54000 <eloi_duchene@orange.fr>
2023-11-06 17:09:36 +0100

    Ajout des actions de bases

---
+++ src/pages/action/Create_Acount.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace iutnc\touiter\Action;
+
+class Create_Acount extends Action
+{
+
+    public function execute(): string
+    {
+        // TODO: Implement execute() method.
+    }
+}
