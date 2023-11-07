ef3ac034eb416fbcb2bb85665336db173cccb382
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-11-07 14:12:48 +0100

    modif index

--- src/pages/main/index.php
+++ src/pages/main/index.php
@@ -12,34 +12,57 @@
     <h1>TOUITEUR</h1>
     <div class="menu navigation">
         <a href="index.php"><p>Profile</p></a>
-        <a href="index.php"><p>Acceuil</p></a>
+        <a href="index.php"><p>Accueil</p></a>
     </div>
     <div class="menu connexion">
-        <a href="index.php"><p>Se connecter</p></a>
-        <a href="index.php"><p>S'inscrire</p></a>
+        <a href="?action=signin"><p>Se connecter</p></a>
+        <a href="?action=signup"><p>S'inscrire</p></a>
     </div>
+    <label>
+        <input type="text" placeholder="Rechercher..">
+    </label>
 </header>
 <body>
+<div class="touite">
+    <div class="infos">
+        <p>Alexis</p>
+        <p>Zott</p>
+    </div>
+    <div class="date">
+        <p>07 / 11 / 2023</p>
+    </div>
+    <div class="message">
+        <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et
+            la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis l
+            es années 1500, quand un imprimeur anonyme assembl
+            a ensemble des morceaux de texte pour réaliser un livre .</p>
+    </div>
+    <div class="interaction">
+        <p>LIKE</p>
+        <p>DISLIKE</p>
+    </div>
+</div>
+
 <?php
 
+require_once '../../../vendor/autoload.php';
+
 use iutnc\touiter\db\ConnexionFactory;
+use iutnc\touiter\dispatch\Dispatcher;
+session_start();
 
-ConnexionFactory::setConfig('./src/classes/conf/config.ini');
+if(!isset($_GET['action'])){
+    $_GET['action'] = "TouiteDisplay";
+}
 
+ConnexionFactory::setConfig('../classes/conf/config.ini');
+
+$d = new Dispatcher();
+$d->run();
 //TODO
+
 
 ?>
 </body>
-<footer>
-    <div>
-        <p>Created by</p>
-        <div id="creator">
-            <p>Alexis Zott</p>
-            <p>Thomas Charpentier</p>
-            <p>Eloi Duchene</p>
-            <p>Baptiste Besoin</p>
-        </div>
-    </div>
-</footer>
 </html>
 
