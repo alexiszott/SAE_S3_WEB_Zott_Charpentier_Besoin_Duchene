617ee7db46ca8809bfafb5d568cf2b7b05b8d2f0
BaptisteBesoin <besoin5u@etu.univ-lorraine.fr>
2023-11-07 10:56:16 +0100

    ajout du vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9030f6d046245fecfb553b63ec0784a37be9a218',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9030f6d046245fecfb553b63ec0784a37be9a218',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
