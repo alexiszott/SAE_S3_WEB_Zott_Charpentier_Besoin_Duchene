335f7cf902b666187b2dd2f673babf3cf5e7f663
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-11-07 10:31:51 +0100

    Ajout html et css dans index

--- src/pages/main/index.php
+++ src/pages/main/index.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr>
+<head>
+    <meta charset=" UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Touiteur</title>
+<link rel="stylesheet" href="../../css/touit.css">
+<link rel="icon" href="./favicon.ico" type="image/x-icon">
+</head>
+<header>
+    <h1>TOUITEUR</h1>
+    <div class="menu navigation">
+        <a href="index.php"><p>Profile</p></a>
+        <a href="index.php"><p>Acceuil</p></a>
+    </div>
+    <div class="menu connexion">
+        <a href="index.php"><p>Se connecter</p></a>
+        <a href="index.php"><p>S'inscrire</p></a>
+    </div>
+</header>
+<body>
 <?php
+
+//TODO
+
+?>
+</body>
+<footer>
+    <div>
+        <p>Created by</p>
+        <div id="creator">
+            <p>Alexis Zott</p>
+            <p>Thomas Charpentier</p>
+            <p>Eloi Duchene</p>
+            <p>Baptiste Besoin</p>
+        </div>
+    </div>
+</footer>
+</html>
+
