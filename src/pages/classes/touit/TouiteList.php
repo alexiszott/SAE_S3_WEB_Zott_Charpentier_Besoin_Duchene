e6f274b2d56c7233b9c2e19426cdf0d3ff59ec2d
Thomas Charpentier <thomas.charpentier9@etu.univ-lorraine.fr>
2023-11-07 15:08:27 +0100

    ajout methode changer liste

--- src/pages/classes/touit/TouiteList.php
+++ src/pages/classes/touit/TouiteList.php
@@ -21,25 +21,49 @@ class TouiteList
         throw new \Exception ("$at: invalid property");
     }
 
-    public function mainTouiteList(){
-        $this->touiteList = [];
-           $pdo = ConnexionFactory::makeConnection();
-        $query = "select idTouite, idImage, texteTouite, datePubli, prenomUtil, nomUtil from touite, util where touite.idUtil=util.idUtil order by datePubli desc limit 10 offset ?";
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(1, $this->nPages);
-        $stmt->execute();
-
-        while ($result = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+    public function creerTouiteListe(\PDOStatement $statement, \PDO $pdo){
+        $statement->execute();
+        while ($result = $statement->fetch(\PDO::FETCH_ASSOC)) {
             if(is_null($result['idImage'])){
-            $this->touiteList[] = new Touite($result['idTouite'],$result['datePubli'], $result['texteTouite'], null, $result['prenomUtil'], $result['nomUtil']);}
+                $this->touiteList[] = new Touite($result['idTouite'],$result['datePubli'], $result['texteTouite'], null, $result['prenomUtil'], $result['nomUtil']);}
             else{
                 $query2 = "select cheminImage from image where idImage = ?";
                 $stmt2 = $pdo->query($query2);
                 $stmt2->bindParam(1, $result['datePubli']);
                 $stmt2->execute();
-                $result2 = $stmt->fetch(\PDO::FETCH_ASSOC);
+                $result2 = $statement->fetch(\PDO::FETCH_ASSOC);
                 $this->touiteList[] = new Touite($result['idTouite'],$result['datePubli'], $result['texteTouite'], $result2['cheminImage'], $result['prenomUtil'], $result['nomUtil']);}
         }
         $pdo=null;
     }
+
+    public function mainTouiteList(){
+        $this->touiteList = [];
+        $pdo = ConnexionFactory::makeConnection();
+        $query = "select idTouite, idImage, texteTouite, datePubli, prenomUtil, nomUtil from touite, util where touite.idUtil=util.idUtil order by datePubli desc limit 10 offset ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(1, $this->nPages);
+        $this->creerTouiteListe($stmt, $pdo);
+    }
+
+    public function userTouiteList(string $email){
+        $this->touiteList = [];
+        $pdo = ConnexionFactory::makeConnection();
+        $query = "select idTouite, idImage, texteTouite, datePubli, prenomUtil, nomUtil from touite, util where touite.idUtil=util.idUtil and util.emailUtil = ? order by datePubli desc limit 10 offset ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(1, $email);
+        $stmt->bindParam(2, $this->nPages);
+        $this->creerTouiteListe($stmt, $pdo);
+    }
+
+    public function tagTouiteList(string $tag){
+        $this->touiteList = [];
+        $pdo = ConnexionFactory::makeConnection();
+        $query = "select idTouite, idImage, texteTouite, datePubli, prenomUtil, nomUtil from touite, util, tag, tag2touite where touite.idUtil=util.idUtil and tag2touite.idTouite=touite.idTouite and tag2touite.idTag=tag.idTag and tag.libelleTag = ? order by datePubli desc limit 10 offset ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(1, $tag);
+        $stmt->bindParam(2, $this->nPages);
+        $this->creerTouiteListe($stmt, $pdo);
+    }
+
 }
