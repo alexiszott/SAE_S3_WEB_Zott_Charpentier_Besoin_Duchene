884284bd4ac61dad60e7d6fbe59958823e068be2
BaptisteBesoin <besoin5u@etu.univ-lorraine.fr>
2023-11-07 16:50:32 +0100

    Ajout des fonctionnalités Ecrire touite et Gestion des tags

--- src/pages/classes/followable/Tag.php
+++ src/pages/classes/followable/Tag.php
@@ -2,6 +2,8 @@
 
 namespace iutnc\touiter\user;
 
+use iutnc\touiter\db\ConnexionFactory;
+
 class Tag
 {
     private int $tagId;
@@ -12,5 +14,26 @@ class Tag
         $this->tagId = $id;
         $this->tagLibelle = $lib;
         $this->tagDescription = $desc;
+    }
+
+    public static function ajouter_tags(array $tags, int $idTouite) : void {
+        $pdo = ConnexionFactory::makeConnection();
+        $sql = "SELECT COUNT(*) count FROM tag WHERE libelleTag = ?";
+        $sqlMaxId = "SELECT MAX(idTag) max FROM tag";
+        foreach ($tags as $v) {
+            $result = $pdo->prepare($sql);
+            $result->bindParam(1, $v);
+            $result->execute();
+            $count = $result->fetch(\PDO::FETCH_ASSOC)['count'];
+            $result = $pdo->prepare($sqlMaxId);
+            $result->execute();
+            $maxId = $result->fetch(\PDO::FETCH_ASSOC)["max"];
+            if ($count === 0) {
+                $sqlInsertTag = "INSERT INTO tag(libelleTag, descriptionTag) VALUES ($v, 'NA')";
+                $pdo->exec($sqlInsertTag);
+                $sqlInsertTag2Touite = "INSERT INTO tag2touite VALUES ($maxId+1, $idTouite)";
+                $pdo->exec($sqlInsertTag2Touite);
+            }
+        }
     }
 }
--- src/pages/classes/touit/Touite.php
+++ src/pages/classes/touit/Touite.php
@@ -29,4 +29,26 @@ class Touite
         throw new \Exception ("$at: invalid property");
     }
 
+    public static function extraire_tags(string $touite) : array|null
+    {
+        $listeTags = null;
+        if (str_contains($touite, '#')) {
+            $listeChaine = str_word_count($touite, 1, '#');
+            function tags($var)
+            {
+                return (str_contains($var, '#'));
+            }
+            // On filtre les valeurs pour retenir uniquement les chaînes contenant un #
+            $listeTags = array_filter($listeChaine, "tags");
+            // Après le filtre on obtien des trous dans le tableau, donc on les supprime
+            $listeTags = array_values($listeTags);
+            // On veut uniquement les mots sans les #
+            foreach ($listeTags as $k => $v) {
+                $listeTags[$k] = ltrim($v, '#');
+            }
+        }
+        return $listeTags;
+    }
+
+
 }
