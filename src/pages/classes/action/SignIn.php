ee30daa9fcd0e377bf52b3f8bd57070a9b729ce6
ED54000 <eloi_duchene@orange.fr>
2023-11-07 17:12:42 +0100

    Création du fichier signIn

--- src/pages/classes/action/SignIn.php
+++ src/pages/classes/action/SignIn.php
@@ -2,11 +2,35 @@
 
 namespace iutnc\touiter\action;
 
+use iutnc\touiter\auth\Auth;
+use iutnc\touiter\exception\AuthException;
+
 class SignIn extends Action
 {
 
     public function execute(): string
     {
-        // TODO: Implement execute() method.
-    }
+        $texte = '';
+
+        if ($this->http_method == 'GET') {
+
+            $texte .= '<form method="post">                        
+                        Email : <input type="email" name="email"><br>
+                        Mot de passe : <input type="password" name="passwd"><br>
+                        <input type = "submit" name = "connect" value = "Connectez-vous">
+                        </form>' ;
+
+        } else if ($this->http_method == 'POST') {
+            $email = $_POST['email'];
+            $mdp=$_POST['passwd'];
+            $var = Auth::authenticate($email, $mdp);
+            if ($var === true) {
+                $texte.="Bienvenue";
+            }
+            }
+            return $texte;
+        }
+
+
+
 }
